e6572fdf8d4b5725b7066270ede3dea780e008a5
YOONIQUE-GARAGE <yooniquegarage@gmail.com>
2021-11-18 23:07:31 +0900

    03_todoComponent_ui

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Todo = () => {
-
+  // 한개의 todo 구성(todo + 버튼 2개)
+  return(
+  <li className="todo_container">
+    <div className="todo">여기에 하나의 투두가 뜹니다</div>
+    <button className="emergency_button">우선</button>
+    <button className="next_button">다음단계로</button>
+  </li>
+  )
 }
 
 export default Todo;
